07b3d4272d9c81d02d6ba1f84934e21db394661b
mritunjayRoy <mroy8202@gmail.com>
2024-12-16 16:44:35 +0530

    updated

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3000/api/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL
+console.log("base url: ", BASE_URL)
 
 // auth endpoints
 export const authEndpoints = {
